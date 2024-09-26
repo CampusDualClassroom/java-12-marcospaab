452d4f3995d3c94c5176af257152123a9e953bde
marcos.padin@outlook.es <you@example.com>
2024-09-26 09:56:29 +0200

    Tarea 12 finalizada

--- src/main/java/com/campusdual/classroom/Exercise12.java
+++ src/main/java/com/campusdual/classroom/Exercise12.java
@@ -2,7 +2,21 @@ package com.campusdual.classroom;
 
 public class Exercise12 {
     public static void main(String[] args) {
-
+        Car c1 = new Car("Citroen", "C3", "Metralla");
+        System.out.println(c1.isTachometerEqualToZero());
+        c1.start();
+        c1.start();
+        c1.accelerate();
+        c1.accelerate();
+        c1.brake();
+        c1.brake();
+        c1.turnAngleOfWheels(42);
+        c1.turnAngleOfWheels(49);
+        c1.setReverse(true);
+        c1.accelerate();
+        c1.setReverse(false);
+        c1.setReverse(true);
+        c1.showDetails();
     }
 
 }
